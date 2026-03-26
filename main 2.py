3be792c604aa089e3ba7e46abe48fc8a2ba418d6
Фыр <danil.erlih@gmail.com>
2026-03-26 12:54:10 +0200

    Home work 2

---
+++ main 2.py
@@ -0,0 +1,14 @@
+def is_palindrome(text):
+
+        text = ''.join(i.lower() for i in text if i.isalnum())
+
+        return text == text[::-1]
+
+
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+
+
+print("ОК")
